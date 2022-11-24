8dbb618f15bcc2f4aced35a7ac2ea7faf914aea7
Chanda Gonet <chandagonet@gmail.com>
2022-11-24 09:23:20 -0500

    up to ajax

--- main.js
+++ main.js
@@ -3,12 +3,25 @@ const contents = document.getElementById('contents');
 const contentParent = contents.parentNode;
 contents.parentNode.removeChild(contents);
 
+const chips = document.getElementById('chips');
+chips.parentNode.removeChild(chips);
+
+const items = document.getElementById('items');
+items.parentNode.removeChild(items);
+
+
+
+
 // ROTATING IMAGE GALLERY
 const inspo = document.createElement("IMG");
 inspo.setAttribute("src", "https://qph.cf2.quoracdn.net/main-qimg-25f9bd5e0a10d7e8822262596dbc2392.webp");
 inspo.setAttribute("width", "auto");
 inspo.setAttribute("height", "400");
 inspo.setAttribute("alt", "The Pulpit Rock");
+inspo.setAttribute("id", "inspo");
+
+
+
     
 contentParent.prepend(inspo);
 
@@ -21,14 +34,28 @@ const gallery = [
     "https://qph.cf2.quoracdn.net/main-qimg-25f9bd5e0a10d7e8822262596dbc2392.webp"
 ]
 
-delay()
+// delay()
 
-function delay(i=0) {
-    setInterval(() => {
-        inspo.setAttribute("src", gallery[i]);
-        i++;
-        if (i === 5) i = 0;
-    }, 5000);
-}
+// function delay(i=0) {
+//     setInterval(() => {
+//         inspo.setAttribute("src", gallery[i]);
+//         i++;
+//         if (i === 5) i = 0;
+//     }, 5000);
+// }
 
+// INSPIRATIONAL WORDS
+const wisdom = document.createElement("h2");
+const wordsOf = document.createTextNode("Sorry YouTube got cancelled! haxx0r chanDUH");
+wisdom.classList.add('colorChange')
+wisdom.appendChild(wordsOf);
+wisdom.setAttribute('id', 'wisdom')
+contentParent.prepend(wisdom);
 
+// HTTP REQUEST
+
+  const someTask = new Promise((resolve, reject) => {
+    let data = fetch('https://twitter.com/kateconger/status/1593649550446628866?ref_src=twsrc%5Etfw');
+    console.log(data)
+  });
+someTask(alert('yes'), alert('no'))
